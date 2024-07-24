faea4634799fad259f690d454d9e42a5ea906746
Cyrnaxx. <65818042+Cyrnax24@users.noreply.github.com>
2024-07-24 13:49:58 +0200

    💎 Modifications

---
+++ assets/config.js
@@ -0,0 +1,33 @@
+const config = {
+    USERNAME: "Cyrnaxx.",
+    PICTURE: "assets/img/profile_pictureup.png",
+
+    DESCRIPTION: "I'm a JavaScript developer, I moderate and administer a lot of communities and large servers. Je parle nativement Français, I speak English, hablo español.",
+
+    BADGES: [
+        { id: "Moderator Programs Alumni", image: "https://cdn.discordapp.com/emojis/1048014221234950154.png" },
+        { id: "HypeSquad Balance", image: "https://cdn.discordapp.com/emojis/902664136897474570.png" },
+        { id: "Active Developer", image: "https://cdn.discordapp.com/emojis/1040373928872382504.png" },
+        { id: "Nitro Sub", image: "https://cdn.discordapp.com/emojis/903018530176397352.png" },
+        { id: "Nitro Boost", image: "https://cdn.discordapp.com/emojis/979198267197906984.gif" },
+        { id: "Originally knows as Cyrnaxx.#9999", image: "https://cdn.discordapp.com/emojis/1109948638068736061.png" },
+        { id: "Completed a Quest", image: "https://cdn.discordapp.com/emojis/1233045314018213990.png" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder5.png", link: "https://discord.com/moderation", name: "Discord Moderator Academy" },
+        { id: "project-2", image: "https://cdn.discordapp.com/emojis/979019753534398474.png", link: "https://discord.com/invite/w5NCQPy6sT", name: "Owner @ P2N" },
+        { id: "project-3", image: "assets/img/placeholder2.gif", link: "https://discord.com/invite/AaucX7B6xB", name: "Owner @ Development Community" },
+        { id: "project-4", image: "assets/img/placeholder1.png", link: "https://discord.gg/5C8Bg8CeMf", name: "Senior Analyst @ Protect" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Cyrnax24" },
+        { class_name: "fab fa-youtube", link: "https://www.youtube.com/channel/UCCt1VLzP3rUOoM0dEayzuKA" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/Cyrnax24" },
+        { class_name: "fab fa-opera", link: "https://Cyrnax24.github.io/" },
+        { class_name: "fab fa-instagram", link: "https://instagram.com/cyrnax_discord24" },
+        { class_name: "fab fa-spotify", link: "https://open.spotify.com/user/phd6s3yf8u7p2xiosz1qrppqc" },
+        { class_name: "fab fa-tiktok", link: "https://www.tiktok.com/@cyrnaxx_discord24" }
+    ],
+}
